ab0363336af1319c126eb04bd715b1369aeff07c
isocyanide10 <b230056@nitsikkim.ac.in>
2024-09-27 16:25:27 +0530

    Adding First C file

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter the value : ");
+    scanf("%d",&x);
+    for(int i=0;i<x;i++)
+    {
+        printf("Hello World !");
+    }
+    return 0;
+}
